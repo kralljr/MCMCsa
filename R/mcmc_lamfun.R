d7726c9dfd1b94d7f1b17168b6aa580e225b2a4f
Jenna Krall <jenna.krall@gmail.com>
2014-10-08 14:15:03 -0400

    Add comments, try and vectorize

--- R/mcmc_lamfun.R
+++ R/mcmc_lamfun.R
@@ -1,21 +1,33 @@
 
 #####
-# random walk for lambda?
+# random walk for lambda
+#dat is data
+#guessvec is current guesses
 lamfun <- function(dat, guessvec)	{
 	
+	
+	#set old guess
 	lamstar <- guessvec[["lamstar"]]
+	
+	#set dimensions
+	L <- nrow(lamstar)
+	P <- ncol(lamstar)
+	
+	#set variance for proposal distribution
 	sd1 <- 0.1
+	
+	#propose
+	newlam <- rnorm(L * P, mean = lamstar, sd = sd1)
 	
 	for(l in 1 : L) {
 		for(p in 1 : P) {
 			#propose
-			newlam <- rnorm(1, lamstar[l, p], sd = sd1)
 			lamstarNEW <- lamstar
-			lamstarNEW[l, p] <- newlam
+			lamstarNEW[l, p] <- newlam[l, p]
 			
 			#compute lhoods
-			lhoodOLD <- lhoodlam(lamstar, dat, guessvec)
-			lhoodNEW <- lhoodlam(lamstarNEW, dat, guessvec)
+			lhoodOLD <- lhoodlam(lamstar, dat, guessvec, p)
+			lhoodNEW <- lhoodlam(lamstarNEW, dat, guessvec, p)
 			
 			#decide whether to accept
 			prob <- min(1, lhoodNEW / lhoodOLD)
@@ -32,15 +44,23 @@ lamfun <- function(dat, guessvec)	{
 
 
 
-
-lhoodlam <- function(lamstar, dat, guessvec) {
+#function to get lhood for lambda
+#lamstar is current guess
+#dat is data
+# guess is other current guesses
+lhoodlam <- function(lamstar, dat, guessvec, p) {
+	
+	#update guess
+	guessvec[["lamstar"]] <- lamstar
 	
 	#likelihood of data
-	guessvec[["lamstar"]] <- lamstar
-	first <- prod(ly(dat, guessvec))
+	first <- ly(dat, guessvec, p)
 	
+	#set prior values
 	mn <- -0.5
 	sd <- sqrt(0.588)
+	
+	#get truncated normal density
 	second <- dtruncnorm(lamstar, a = 0, mean = mn, sd = sd)
 	
 	first * second
